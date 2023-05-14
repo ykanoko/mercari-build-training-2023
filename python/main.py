74dddc80013601c457757f474bdf23ccd375ff7f
ykanoko <kano7101d.pg@gmail.com>
2023-05-14 20:06:03 +0900

    STEP3-2

--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
 import os
+import json
 import logging
 import pathlib
 from fastapi import FastAPI, Form, HTTPException
@@ -9,23 +10,44 @@ app = FastAPI()
 logger = logging.getLogger("uvicorn")
 logger.level = logging.INFO
 images = pathlib.Path(__file__).parent.resolve() / "images"
-origins = [ os.environ.get('FRONT_URL', 'http://localhost:3000') ]
+origins = [os.environ.get('FRONT_URL', 'http://localhost:3000')]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=False,
-    allow_methods=["GET","POST","PUT","DELETE"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
+
+PATH_ITEMS = "./data/items.json"
+ENCODING = "utf-8"
+
 
 @app.get("/")
 def root():
     return {"message": "Hello, world!"}
 
+
 @app.post("/items")
-def add_item(name: str = Form(...)):
-    logger.info(f"Receive item: {name}")
-    return {"message": f"item received: {name}"}
+def add_item(name: str = Form(...), category: str = Form(...)):
+    logger.info(f"Receive item: {name, category}")
+    try:
+        #ファイルが存在する場合
+        with open(PATH_ITEMS, 'r', encoding=ENCODING) as f:
+            data = json.load(f)
+            data['items'].append({"name": name, "category": category})
+    except FileNotFoundError:
+        #ファイルが存在しない場合
+        print("File not found.")
+        data = {"items": [{"name": name, "category": category}]}
+    try:
+        with open(PATH_ITEMS, 'w', encoding=ENCODING) as f:
+            json.dump(data, f)
+        pass
+    except Exception as error:
+        print(error)
+    return {"message": f"item received: {name, category}"}
+
 
 @app.get("/image/{image_filename}")
 async def get_image(image_filename):
